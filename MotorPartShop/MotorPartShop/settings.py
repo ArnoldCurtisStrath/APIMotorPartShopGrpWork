6597fba5eaf5baead86ad206070fc52e69ecea83
Arnold Curtis <arnoldmbici@gmail.com>
2025-11-02 14:07:15 +0300

    Add basic motor part shop skeleton with Basic templates and views

--- MotorPartShop/MotorPartShop/settings.py
+++ MotorPartShop/MotorPartShop/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'MotorPartShop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'MotorPartShop' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
