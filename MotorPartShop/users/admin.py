5ae9eeb11b73de141ac128e5a11abc1eca66e067
thisisandrew254 <andrew.kwendo@strathmore.edu>
2025-11-11 10:43:53 +0300

    feature/user-management)

--- MotorPartShop/users/admin.py
+++ MotorPartShop/users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import UserProfile
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'phone_number', 'city', 'created_at']
+    search_fields = ['user__username', 'user__email']
+    list_filter = ['city', 'created_at']
+    readonly_fields = ['created_at']
+    ordering = ['-created_at']
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'phone_number', 'address', 'city')
+        }),
+        ('Timestamps', {
+            'fields': ('created_at',),
+            'classes': ('collapse',),
+        }),
+    )
